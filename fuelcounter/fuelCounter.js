9496b8583fdbb816e16f9bc52e29377457737d17
Łukasz Lebioda <lukasz.leb@gmail.com>
2022-11-28 22:06:20 +0100

    new js program: a fuel counter

---
+++ fuelcounter/fuelCounter.js
@@ -0,0 +1,41 @@
+
+let route;
+let price;
+let averageConsumption;
+let persons;
+
+let totalConsumption;
+
+let resultUnrounded;
+let resultRounded;
+
+document.getElementById("buttonGasoline").onclick = function(){
+    
+   route = document.getElementById("routeGasoline").value;
+   price = document.getElementById("priceGasoline").value;
+   averageConsumption = document.getElementById("averageConsumptionGasoline").value;
+   persons = document.getElementById("personsGasoline").value;
+
+   if(route === "" || price === ""|| averageConsumption === "" || persons === ""){
+      window.alert("Looks like You skipped some questions. Please review the form and make sure all the answers are provided");
+    
+   } else if (route <= 0 || price <= 0 || averageConsumption <= 0 || persons <= 0) {
+       window.alert("Some answers You've provided are zero or numbers below zero. I'm sure You've done this undeliberately :) Please correct this however, because I want to move on with all the counting.");
+   } else {
+      
+      totalConsumption = (route * averageConsumption) / 100
+
+      resultUnrounded = totalConsumption * price / persons
+      resultRounded = resultUnrounded.toFixed(2)
+       
+        document.getElementById("providedRoute").innerHTML = `${route} km`
+        
+        document.getElementById("providedPrice").innerHTML = `${price} (e.g. EUR)` 
+        document.getElementById("providedAverageConsumption").innerHTML = `${averageConsumption} / 100km`  
+        document.getElementById("providedPersonsNumber").innerHTML = `${persons} person(s)`
+        document.getElementById("resultGasoline").innerHTML = `${resultRounded} (e.g. EUR / person(s))`
+
+        }
+}
+
+module.exports = fuelCounter;
---
+++ fuelcounter/readme.md
@@ -0,0 +1,1 @@
+It's just a simple program to count how much each member of a vehicle trip should pay for gasoline, depending on the distance, fuel price, car average fuel consumption  and number of persons.
