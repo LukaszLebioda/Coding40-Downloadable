d8368fc114d82c7ec7d92e49dd3564a7da2ba815
LukaszLebioda <lukasz.leb@gmail.com>
2022-12-23 08:37:14 +0100

    final env test

--- aa varia/TestLink(config).php
+++ aa varia/TestLink(config).php
@@ -6,27 +6,39 @@ https://www.youtube.com/watch?v=f9q65Qov9hI&t=689s
 
 3. Copy testlink folder into xampp/htdocs.
 
-4. Replace two directories in config.inc.php file with:
+4. Replace two directories in testlink/config.inc.php file with:
+
+testlink/logs:
 
 $tlCfg->log_path = 'C:/xampp/htdocs/testlink/logs/'; /* unix example */
 if (($lp = getenv('TESTLINK_LOG_PATH'))) {
   $tlCfg->log_path = trim($lp);
 }
 
+testlink/upload_area:
+
 $g_repositoryPath = 'C:/xampp/htdocs/testlink/upload_area/';  /* unix example */
 if (($upa = getenv('TESTLINK_UPLOAD_AREA'))) {
   $g_repositoryPath = trim($upa);
 }
 
-5. 	Database admin login: root / Database admin password: ""
-	TestLink DB login: admin / TesLink DB login: admin
+5. Uruchom localhost/testlink - "New installation".
+
+6. 	Database admin login: root 
+	Database admin password: ""
 	
-6. 	Localhost/testlink: enter application.
+	TestLink DB login: admin
+	TesLink DB login: admin
+	
+7. 	Localhost/testlink: login to application (admin / admin).
+
 	Create new project: Name, e.g. Test Project.
 	Prefix: e.g. TP.
 	Description: optional.
 	Create.
+	
 	Test Plan Management.
 	Create.
 	Name: e.g.Testplan.
 	Description: optional.
+	Active, Public - both checkboxes checked!
